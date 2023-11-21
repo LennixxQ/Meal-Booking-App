199ac8d606513b66fb38c8d796c16ba84bc17ffd
LennixxQ <chauhanvivek1750@gmail.com>
2023-11-21 10:09:23 +0530

    first commit

---
+++ server/MealBooking/src/main/java/com/project/MealBooking/MealBookingApplication.java
@@ -0,0 +1,13 @@
+package com.project.MealBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealBookingApplication.class, args);
+	}
+
+}
---
+++ server/MealBooking/src/test/java/com/project/MealBooking/MealBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MealBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
